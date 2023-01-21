f3108c6b81efa60b9b5442956839cfa3581016f4
Ravina Vala <vala8460@gmail.com>
2023-01-22 01:53:34 +0530

    created and load home page component

---
+++ react-app/src/components/Home/HomePageContent.js
@@ -0,0 +1,12 @@
+import classes from "./HomePageContent.module.css"
+
+const HomePageContent = () => {
+return ( 
+   <section className={classes.homewrapper}>
+     <h1>THis is my react home component</h1>
+   </section>
+ );
+
+}
+
+export default HomePageContent;
